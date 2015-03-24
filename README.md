10bb988b2a74e8262267b38221a7e58e411d76a8
codedust <codedust@users.noreply.github.com>
2015-03-24 15:11:57 +0100

    Add 3rd parameter multiplying the fun.
    PS: No need for 'for' loops. [Quirks mode activated]

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ shellsay
 a simple script that adds vocal cords to your shell
 
 ### How to use
+```
+say "Hello World"       #Default is "en"
+say "Hello World" fr    #French pronunciation
+say "Hello World" fr 20 #French robot ;)
+```
+
+### How to setup
 
 1) Load the script manually with source:
 ```
--- shellSay.sh
+++ shellSay.sh
@@ -15,6 +15,14 @@ urlencode() {
 
 say() {
   if [ -z $2 ]; then lang="en"; else lang="$2"; fi
-  curl --user-agent "Mozilla/5.0 (X11; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0" --url "http://translate.google.de/translate_tts?tl=$lang&q="$(urlencode "$1")"" -o googlevoice.mp3
-  play googlevoice.mp3
+  curl --user-agent "Mozilla/5.0 (X11; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0" --url "http://translate.google.de/translate_tts?tl=$lang&q="$(urlencode "$1")"" -o text2say.mp3
+  if [ -z $3 ]; then times=1; else times="$3"; fi
+  youdont $times;
+}
+
+youdont(){
+  if [ $1 -ne 0 ]; then
+    one="$1"
+    youdont $((one-1)) | play text2say.mp3
+  fi
 }
